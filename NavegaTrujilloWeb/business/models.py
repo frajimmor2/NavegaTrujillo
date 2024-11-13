777eef7caf68be6e89a052f98c04c754c3ffb864
frajimmor2 <frajimmor2@alum.us.es>
2024-11-13 21:14:28 +0100

    Actividad2 completada

--- NavegaTrujilloWeb/business/models.py
+++ NavegaTrujilloWeb/business/models.py
@@ -1,7 +1,7 @@
 from django.utils import timezone
 from django.db import models
 from django.core.validators import MinValueValidator, MaxLengthValidator, MinLengthValidator
-
+#Todo ok
 # Create your models here.
 
 class Port(models.Model):
