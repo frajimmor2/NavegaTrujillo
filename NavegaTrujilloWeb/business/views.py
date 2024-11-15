992d6f44f9fb3056f17b766cd2641cfc0aa854db
JoseGTD <josgardel8@alum.us.es>
2024-11-15 14:25:57 +0100

    Funcionalidad template base añadida

--- NavegaTrujilloWeb/business/views.py
+++ NavegaTrujilloWeb/business/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 def home(request):
 
-    content = open("../NavegaTrujilloWeb/business/view_templates/home_view.html")
+    content = open("./business/view_templates/home_view.html")
     tplt = Template(content.read())
     content.close()
 
