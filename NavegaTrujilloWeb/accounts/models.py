8fce96e5f5bfae8d83c0ba2c0ec98c5e27f44bc7
JoseGTD <josgardel8@alum.us.es>
2024-11-17 21:03:01 +0100

    register hecho

--- NavegaTrujilloWeb/accounts/models.py
+++ NavegaTrujilloWeb/accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    
+    name = models.CharField(max_length=50,validators=[MinLengthValidator(0)], blank=False)
+    surname = models.CharField(max_length=100,validators=[MinLengthValidator(0)], blank=False)
+    email = models.CharField(max_length=50,validators=[MinLengthValidator(0)], blank=False)
+
+    def __str__(self):
+        return f"{self.name} {self.surname} : {self.email}"
+
+
--- NavegaTrujilloWeb/accounts/views.py
+++ NavegaTrujilloWeb/accounts/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, authenticate 
+from .forms import CustomUserCreationForm
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
