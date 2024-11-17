daaceae361e60e705e8b3e96d6a2b25365c663de
sfernandez07 <sanferrom@alum.us.es>
2024-11-17 16:22:19 +0100

    Arreglo import

--- NavegaTrujilloWeb/login/urls.py
+++ NavegaTrujilloWeb/login/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from profiles.views import CustomLoginView
+from login.views import CustomLoginView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
