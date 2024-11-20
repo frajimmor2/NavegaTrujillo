6e560441e38efe5f7bf42c556e9f260d0bdc965e
JoseGTD <josgardel8@alum.us.es>
2024-11-20 12:02:26 +0100

    Ahora se inicia sesión con el email

--- NavegaTrujilloWeb/accounts/views.py
+++ NavegaTrujilloWeb/accounts/views.py
@@ -1,11 +1,16 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render,redirect
 from django.contrib.auth import login, authenticate 
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm,LoginForm
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("home")
     template_name = "registration/signup.html"
+
+class LoginView(LoginView):
+    form_class = LoginForm
+    success_url = reverse_lazy("home")
+    template_name = "registration/login.html"
