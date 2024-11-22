2dcc83e3cb60fdda8ee90c6646fc89f07d9f6f9e
sfernandez07 <sanferrom@alum.us.es>
2024-11-22 16:30:13 +0100

    Añadir capitanes a la cesta y coste adicional de estos

--- NavegaTrujilloWeb/business/views.py
+++ NavegaTrujilloWeb/business/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Ship, Port, Shopping_basket, Client
 from django.utils import timezone
+from datetime import datetime
 
 
 def home(request):
@@ -35,8 +36,19 @@ def cookieCart(request):
     cart = ships_dict'''
         
     items = []
-    order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
+    order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False, 'captain_amount': 0, 'captain_cost': 0, 'total_with_captain': 0}
     cartItems = order['get_cart_items']
+
+    rental_start_date = request.GET.get('rental_start_date', None)
+    rental_end_date = request.GET.get('rental_end_date', None)
+    rental_days = 0
+
+    # Este if calcula los dias alquilados y si no esta especificado el rango de fechas, se pone por default 1 dia
+    if rental_start_date and rental_end_date:
+            rental_days = (datetime.strptime(rental_end_date, "%Y-%m-%d") - datetime.strptime(rental_start_date, "%Y-%m-%d")).days
+            rental_days = max(1, rental_days)  # Hace que el mínimo de días sea 1
+    else:
+        rental_days = 1
     
     for i in cart:
         
@@ -51,7 +63,15 @@ def cookieCart(request):
 
         except:
             pass
-            
+
+    captain_amount = cart.get('captain_amount', 0)
+    if captain_amount > cartItems:  # Este if es el que limita la cantidad de capitanes a la cantidad de barcos
+        captain_amount = cartItems  
+
+    order['captain_amount'] = captain_amount
+    order['captain_cost'] = captain_amount * 120 * rental_days # Aqui guarda el precio de los capitanes para invocarlo usando order['coste_capitan']
+    order['total_with_captain'] = order['get_cart_total'] + order['captain_cost']
+
     return {"items": items, "order": order, "cartItems": cartItems, "ships": Ship.objects.all()}
 
 def cart(request):
