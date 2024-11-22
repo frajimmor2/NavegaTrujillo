93c01979eb3f5ea720b78afb01c34599cf098d50
claudiocortescarrasco <clacorcar@alum.us.es>
2024-11-22 17:56:46 +0100

    Creacion de página para que el cliente pueda añadir puertos

--- NavegaTrujilloWeb/business/urls.py
+++ NavegaTrujilloWeb/business/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name="home"),
     path('', views.home, name="home"),
+    path('add-port/', views.add_port, name="add_port"),
 ]
--- NavegaTrujilloWeb/business/views.py
+++ NavegaTrujilloWeb/business/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import Template, Context
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
+from .models import Port
 
 def home(request):
     return render(request,"./business/home_view.html")
+
+
+
+@login_required
+def add_port(request):
+    # Verifica si el usuario tiene permiso de staff
+    if not request.user.is_staff:
+        return HttpResponseForbidden("No tienes permiso para realizar esta acción.")
+
+    if request.method == "POST":
+        ubication = request.POST.get("ubication")
+        if ubication:
+            Port.objects.create(ubication=ubication)
+            return redirect("home")  # Redirige al usuario al inicio después de añadir el puerto
+
+    return render(request, "./business/add_port.html")
+
