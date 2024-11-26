5259041f75908c71eb5fd900def724873566bc72
sfernandez07 <sanferrom@alum.us.es>
2024-11-26 19:46:52 +0100

    Cambios en settings para el PAAS

--- NavegaTrujilloWeb/settings.py
+++ NavegaTrujilloWeb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-21b-bxa@#)20u5pi#oz+()brb91&kbj4dt4!$5m)2idxbkdi!q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'https://proyectonavegatrujillo.pythonanywhere.com']
 
 
 # Application definition
