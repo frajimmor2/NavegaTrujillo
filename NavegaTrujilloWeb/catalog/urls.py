5b96dd60ba5ef1f2946026d7531008c575ea6244
sfernandez07 <sanferrom@alum.us.es>
2024-12-04 21:14:47 +0100

    <3

--- NavegaTrujilloWeb/catalog/urls.py
+++ NavegaTrujilloWeb/catalog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('catalog/ship/<int:ship_id>/',views.show, name="show"),
     path('catalog/ship/<int:ship_id>/reserva',views.reservation, name="reservation"),
     path('catalog/results/', views.filtered_list, name="filter"),
+    path('edit/<int:ship_id>/', views.edit_ship, name='edit_ship'),
+    path('delete/<int:ship_id>/', views.delete_ship, name='delete_ship'),
 ]
